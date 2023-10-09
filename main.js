d781df921b092c3987ae2b0844100ae705539b6c
JinHaGyung <wlsgkrud96@gmail.com>
2023-10-09 09:52:29 +0900

    #3 쌓이면서 상호작용

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ var game = new Phaser.Game(config);
 var players;
 var speed = 4;
 var player;
+var newFox;
 
 //점수
 var score = 0;
@@ -38,6 +39,14 @@ var scoreText;
 
 //목숨
 var lives = 3;
+
+//카운트
+let count = 0;
+//충돌 판정용
+var collisionOccurred = false;
+
+//카메라 움직임
+var cameraY = false;
 
 
 // 게임 리소스(캐릭터 이미지,배경)
@@ -57,50 +66,102 @@ function create() {
   //배경 이미지(배경,기본 캐릭터1)
   var backgroundImage = this.add.image(0, 0, 'sky');
   backgroundImage.setOrigin(0, 0.45); 
-  var fox = this.physics.add.image(0, 0, 'fox');
+  
+  var fox = this.physics.add.image(0, 0, 'fox').setImmovable(true);
   fox.setOrigin(-4, -3.3); 
   fox.setScale(0.3); 
 
-  //캐릭터 기본 세팅
+  //움직이는 여우 기본 세팅
   player = this.physics.add.sprite(400, 500, 'fox');
   player.setOrigin(-1.5, -1); 
   player.setScale(0.3); 
   //마우스 클릭 가능하게 세팅하기
   player.setInteractive();
-  
+
+
   //클릭시 정지 후 던지기
-  player.on('pointerdown', clickPlayer);
-  //점수
+  player.on('pointerdown', clickPlayer,this);
+
+  //UI
   scoreText = this.add.text(16, 16, `Score: ${score}`, { fontSize: '32px', fill: '#fff' });
+  scoreText.setScrollFactor(0);
+  livesText = this.add.text(1000, 16, `Lives: ${lives}`, { fontSize: '32px', fill: '#fff' });
+  livesText.setScrollFactor(0);
+
+  //충돌 적용
+  this.physics.add.collider(fox, players, onCollision, null, this);
+
+}
+function onCollision(fox,players) {
+  if (!collisionOccurred) {
+    // 충돌 시 카운트,점수 증가
+    count += 1;
+    score += 100;
+    collisionOccurred = true;
+    players.body.collideWorldBounds = false;
+    fox.body.collideWorldBounds = false;
+  }
 }
 
-  // 게임 상태를 업데이트(게임 로직)
+// 게임 상태를 업데이트(게임 로직)
 function update() {
   // 캐릭터가 자동으로 좌우로 움직임
   player.x += speed;
+
   // 화면 경계에 닿으면 방향을 바꿈
   if (player.x >= config.width - player.x/2|| player.x <=  -config.width/10) {
      speed *= -1; // 방향 전환
+
   }
   //던진 후 내려오기
-  if (player.y <= -50) {
-    console.log("도달")
-    player.setVelocityY(500);   
+  var lastObject = players.children.entries[players.children.entries.length-1];
+  var underObject = players.children.entries[players.children.entries.length-2];
+  if (lastObject) {
+    if(lastObject.y <= 200-(count*130)){
+      lastObject.setVelocityY(1000);
+      scoreText.setText('Score: ' + score);
+      this.physics.add.collider(lastObject, underObject,(lastObject,underObject)=>{
+          lastObject.setVelocityY(0);
+          underObject.setImmovable(false);
+          if (!collisionOccurred) {
+            // 충돌 시 카운트,점수 증가
+            count++;
+            score += 100;
+            collisionOccurred = true;
+
+          }
+          if (!cameraY) {
+            //쌓일때 카메라 이동
+            cameraY = true;
+            this.cameras.main.scrollY -= 140;
+            player.y -= 140;
+            speed = Math.abs(speed) + 1;
+          }
+      });
+    }
   }
-  // 카메라의 y 좌표를 스크롤하면서 이미지를 올려다보게 함
-  //this.cameras.main.scrollY -= 1; // 스크롤 속도 및 방향을 조절할 수 있음
+  //떨어진 물체 제거
+  players.getChildren().forEach(function(newFox) {
+    if (newFox.y > game.config.height) {
+      newFox.destroy();
+      lives--;
+      livesText.setText('Lives: ' + lives);
+    }
+  });
 }
 
 //클릭 후 정지 후 던지기 함수
-function clickPlayer(){
-  speed = 0;
-  player.setVelocityY(-500);
+function clickPlayer(pointer){
+  collisionOccurred = false;
+  cameraY = false;
+  newFox = players.create(pointer.x, player.y, 'fox');
+  newFox.setScale(0.3);
+  newFox.setVelocityY(-1000);
 
-  var newPlayer = players.create(400, 500, 'fox').setCollideWorldBounds(true);
-  newPlayer.setOrigin(-1.5, -1.7); 
-  newPlayer.setScale(0.3); 
-  newPlayer.setInteractive(); // 클릭 가능하게 설정
 
-  console.log(players.children)
-
+  //움직이는 여우 비활성화
+  player.setVisible(false);
+  setTimeout(function() {
+    player.setVisible(true);
+  }, 1000);
 }
